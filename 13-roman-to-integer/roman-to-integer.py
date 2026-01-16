ee4042c2db261924647463e93010bd592d199957
shimanzu <potoschimateirares@gmail.com>
2026-01-16 23:03:18 +0200

    Time: 1 ms (87.60%) | Memory: 19.5 MB (11.75%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -10,21 +10,16 @@ class Solution:
             'M' : 1000
         }
         sum = 0 
-
-        def value_of_character(chr):
-            return Numerals[chr]
-
         i = len(s)-1
         while i >= 0:
             if i == 0:
-                sum += value_of_character(s[i])
+                sum += Numerals[s[i]]
                 i -= 1
-            elif value_of_character(s[i]) > value_of_character(s[i-1]):
-                sum += (value_of_character(s[i]) - value_of_character(s[i-1]))
+            elif Numerals[s[i]] > Numerals[s[i-1]]:
+                sum += (Numerals[s[i]] - Numerals[s[i-1]])
                 i -= 2
             else:
-                sum += value_of_character(s[i])
-                print(value_of_character(s[i]))
+                sum += Numerals[s[i]]
                 i -= 1
         
         return sum
